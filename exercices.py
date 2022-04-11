ce0624ae9e01cd3fe9ca1d6188dd7790075d251c
ethannn1 <76067923+ethannn1@users.noreply.github.com>
2022-04-11 10:19:54 +0200

    Update exercices.py

--- exercices.py
+++ exercices.py
@@ -21,3 +21,30 @@ valeurs = [34, 56, 89, 134, 152, 250, 87, -34, -187, -310]
 attendu = [34, 56, 89, 134, 150, 150, 87, -34, -150, -150]
 resultat = ecrete(valeurs, -150, 150)
 assert attendu == resultat, f"Erreur avec ecrete({valeurs})"
+
+def inverse_chaine(chaine):
+    resultat = ""
+    for caractere in chaine:
+        resultat = caractere + resultat
+    return resultat
+
+def est_palindrome(chaine):
+    inverse = inverse_chaine(chaine)
+    return inverse == chaine
+
+def est_palindromique(nombre):
+    chaine = str(nombre)
+    return est_palindrome(chaine)
+
+
+# tests
+
+assert inverse_chaine('bac') == 'cab'
+
+assert not est_palindrome('NSI')
+
+assert est_palindrome('ISN-NSI')
+
+assert not est_palindromique(214312)
+
+assert est_palindromique(213312)
